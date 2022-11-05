2ca6b2a82f401baf622f0f77cb7f7ef7e0da68c9
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-05 00:33:23 -0300

    320px 450px 600px done

--- src/componentes/Introducao/Index.jsx
+++ src/componentes/Introducao/Index.jsx
@@ -1,5 +1,7 @@
 import './Introducao.css'
 import celular from "../../assets/celulares.svg"
+import celular2 from "../../assets/celulares320.svg"
+import celular3 from "../../assets/celulares500.svg"
 import Botao from '../Botao';
 
 
@@ -19,6 +21,14 @@ const Introducao = () => {
                 <div className="botao-intro">
                     <Botao >Baixar Agora</Botao>
                 </div>
+            </div>
+
+            <div className="img-introduction-hidden">
+                <img src={celular2} alt="Telas RubBank 320px" />
+            </div>
+
+            <div className="img-introduction-hidden2">
+                <img src={celular3} alt="Telas RubBank 500px" />
             </div>
 
             <div className="img-introduction">
