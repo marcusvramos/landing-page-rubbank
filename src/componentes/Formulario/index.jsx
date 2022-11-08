c5f9129e811f654a7bde88e7ee465f91df03f479
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-08 00:49:59 -0300

    forms

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
-import CampoTexto from '../CampoTexto'
-import './Formulario.css'
 import image_form from '../../assets/form-image.svg'
 import logo_form from '../../assets/logo-form.svg'
 import logo2 from '../../assets/rublogo-form.svg'
+import CampoTexto from '../CampoTexto'
+import './Formulario.css'
 
 
 const Formulario = (props) => {
 
-    const [nome, setNome] = useState('') // hook (gancho) -- valor atual, e o novo valor a ser SETADO
-    const [email, setEmail] = useState('')
 
-    const aoSalvar = (evento) => {
-        evento.preventDefault() // previnir o comportamento padrão do navegador -- 'Submete e Atualiza'
-        props.aoUsuarioCadastrado({
-            nome,
-            email
-        })
-    }
+
 
     return (
         <div className='container-form' id="form-contato">
@@ -28,7 +19,7 @@ const Formulario = (props) => {
             <h2 className='segundo-titulo-hidden' >Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
             <section className='formulario formatacao' >
                 <form id="form" name="contact"
-                    onSubmit={aoSalvar} netlify> 
+                     netlify> 
                     <img src={logo2} alt="logo RubBank" className='img-form-hidden' />
 
                     <h2 className='segundo-titulo'>Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
@@ -36,14 +27,17 @@ const Formulario = (props) => {
                     required={true} 
                     placeholder="Digite seu Nome"
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}
+                    type="text"
+                    name="name"
+                    
                     />
 
                     <CampoTexto 
                     required={true} 
                     placeholder="Digite seu Email"
                     valor={email}
-                    aoAlterado={valor => setEmail(valor)}
+                    type="email"
+                    name="email"
                     />
 
                     <p>O Banco Digital perfeito para você!</p>
