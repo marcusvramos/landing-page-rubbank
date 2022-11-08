227e4d3b1a3c398ca9a1d9b813334c39142a6ba5
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-08 09:25:03 -0300

    forms

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -12,7 +12,7 @@ const Formulario = (props) => {
         const myForm = event.target;
         const formData = new FormData(myForm);
         
-        fetch("application/x-www-form-urlencoded", {
+        fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(formData).toString(),
