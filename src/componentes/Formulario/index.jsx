4d37067fdaeefdbde8fcdb412a47f1f59e1275be
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-08 07:59:48 -0300

    consertando form

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import CampoTexto from '../CampoTexto'
 import './Formulario.css'
 import image_form from '../../assets/form-image.svg'
 import logo_form from '../../assets/logo-form.svg'
@@ -7,17 +5,6 @@ import logo2 from '../../assets/rublogo-form.svg'
 
 
 const Formulario = (props) => {
-
-    const [nome, setNome] = useState('') // hook (gancho) -- valor atual, e o novo valor a ser SETADO
-    const [email, setEmail] = useState('')
-
-    const aoSalvar = (evento) => {
-        evento.preventDefault() // previnir o comportamento padrão do navegador -- 'Submete e Atualiza'
-        props.aoUsuarioCadastrado({
-            nome,
-            email
-        })
-    }
 
     return (
         <div className='container-form' id="form-contato">
@@ -28,26 +15,25 @@ const Formulario = (props) => {
             <h2 className='segundo-titulo-hidden' >Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
             <section className='formulario formatacao' >
                 <form id="form" 
-                    onSubmit={aoSalvar}> 
+                    onSubmit="submit"> 
+                    
                     <img src={logo2} alt="logo RubBank" className='img-form-hidden'/>
 
                     <h2 className='segundo-titulo'>Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
-                    <CampoTexto 
-                    required={true} 
-                    placeholder="Digite seu Nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                    />
 
-                    <CampoTexto 
-                    required={true} 
-                    placeholder="Digite seu Email"
-                    valor={email}
-                    aoAlterado={valor => setEmail(valor)}
-                    />
+                    <div className="campo-texto formatacao">
+                        <label></label>
+                        <input placeholder="Digite seu Nome" required="true" type="text" />
+                    </div>
+
+
+                    <div className="campo-texto formatacao">
+                        <label></label>
+                        <input placeholder="Digite seu Email" required="true" type="email" />
+                    </div>
+
 
                     <p>O Banco Digital perfeito para você!</p>
-
                     <div className='container-botao'>
                         <button className='botao-form-hidden'>
                             Enviar
