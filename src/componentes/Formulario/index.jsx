526551b791488db6b73ee933f8694c77323efccb
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-08 09:22:26 -0300

    forms

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -12,7 +12,7 @@ const Formulario = (props) => {
         const myForm = event.target;
         const formData = new FormData(myForm);
         
-        fetch("https://rubbank.netlify.app/", {
+        fetch("application/x-www-form-urlencoded", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(formData).toString(),
