4fd3b934b92ef40c23e50d77673ffc9e86f7e900
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-07 23:31:14 -0300

    forms

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import CampoTexto from '../CampoTexto'
 import './Formulario.css'
 import image_form from '../../assets/form-image.svg'
@@ -8,16 +7,6 @@ import logo2 from '../../assets/rublogo-form.svg'
 
 const Formulario = (props) => {
 
-    const [nome, setNome] = useState('') // hook (gancho) -- valor atual, e o novo valor a ser SETADO
-    const [email, setEmail] = useState('')
-
-    /*const aoSalvar = (evento) => {
-        evento.preventDefault() // previnir o comportamento padrão do navegador -- 'Submete e Atualiza'
-        props.aoUsuarioCadastrado({
-            nome,
-            email
-        })
-    }*/
 
     return (
         <div className='container-form' id="form-contato">
@@ -28,42 +17,46 @@ const Formulario = (props) => {
             <h2 className='segundo-titulo-hidden' >Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
             <section className='formulario formatacao' >
                 <form 
-                    name="contato v1"
+                    name="contact v1"
                     method="post"
                     data-netlify="true"
                     id="form" 
-                    onSubmit="submit"> 
+                    onSubmit="submit" > 
+
                     <img src={logo2} alt="logo RubBank" className='img-form-hidden'/>
 
-                    <input type="hidden" name="form-name" value="contato v1"/>
+                    <input type="hidden" name="form-name" value="contact v1"/>
 
                     <h2 className='segundo-titulo'>Preencha o Formulário para ficar por dentro das novidades da RubBank!</h2>
+
                     <CampoTexto 
+                    type="text"
                     required={true} 
-                    name="nome"
+                    name="first-name"
                     placeholder="Digite seu Nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
+                    
+                    
                     />
 
                     <CampoTexto 
+                    type="email"
                     required={true} 
                     name="email"
                     placeholder="Digite seu Email"
-                    valor={email}
-                    aoAlterado={valor => setEmail(valor)}
+
+                    
                     />
 
                     <p>O Banco Digital perfeito para você!</p>
 
-                    <div type="submit" className='container-botao'>
-                        <button className='botao-form-hidden'>
-                            Enviar
-                        </button>
-                        <button type="submit" className='botao-form'>
-                            Enviar Formulário
-                        </button>
-                    </div>
+                    
+                    <button className='botao-form-hidden' type="submit">
+                        Enviar
+                    </button>
+                    <button type="submit" className='botao-form'>
+                        Enviar Formulário
+                    </button>
+                    
                 </form>
             </section>
         </div>
