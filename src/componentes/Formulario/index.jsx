4d50c6bf608ef365307468749159c5fe31bcb507
marcus-rubcube <marcus.vinicius@rubcube.com>
2022-11-08 09:36:13 -0300

    forms

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -33,6 +33,7 @@ const Formulario = (props) => {
                     onSubmit={handleSubmit} 
                     name="contact" 
                     method="POST" 
+                    action="/path"
                     netlify> 
                         <img src={logo2} alt="logo RubBank" className='img-form-hidden'/>
             
